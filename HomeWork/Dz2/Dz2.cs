595428917ac415ce665150ff5c03362e68e62419
Arthur Maslauskas <mickeyarthurfirst@gmail.com>
2023-06-10 18:42:50 +1100

    Сделал задачу на сравнение 3 чисел

---
+++ HomeWork/Dz2/Dz2.cs
@@ -0,0 +1,38 @@
+﻿// * *Задание 2:**Напишите программу, 
+// которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Clear();
+Console.WriteLine("Введите Число A:");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Число B:");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Число C:");
+int numberC = int.Parse(Console.ReadLine());
+if (numberA > numberB) 
+{
+    Console.WriteLine("1)Число A Больше Чем B");
+}
+else
+{
+    Console.WriteLine("1)Число B Больше Чем A");
+}
+if (numberB > numberC)
+{
+    Console.WriteLine("2)Число B Больше Чем С");
+}
+else
+{
+    Console.WriteLine("2)Число C Больше Чем В");
+}
+if (numberC > numberA)
+{
+    Console.WriteLine("3)Число C Больше Чем A");
+}
+else
+{
+    Console.WriteLine("Число A Больше С");
+}
+Console.ReadKey();
